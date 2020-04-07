21e87dd0be89d366cc5395388a0d6e29fa232d1f
thebuildhub <joshi.mohit1221@gmail.com>
2020-04-07 17:57:33 +0530

    added menu button and section box design

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,10 +1,26 @@
 console.log("hello from header.js");
 
-const btn = document.querySelector('.menu-bar');
+const btn = document.querySelector('.menu-btn');
+const bar = document.querySelector('.menu-bar');
 const menu = document.querySelector('.menu');
 console.log(btn);
 console.log(menu);
 
-btn.addEventListener('click',e =>{
+
+
+btn.addEventListener('click', e =>{
     menu.classList.toggle('open');
-})
+    if(menu.classList.contains('open')){
+
+        bar.classList.add('menu-bar-close');
+        bar.classList.remove('menu-bar-open');
+
+    }
+    else{
+        bar.classList.add('menu-bar-open');
+        bar.classList.remove('menu-bar-close');
+
+    }
+});
+
+
